b6a704c877b25218b36aed59fff9c4adfcedc7cb
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-08-23 17:10:56 +0100

    using signal dispatcher to create profile from user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=45,null=True , blank=True)
+    address = models.CharField(max_length=100 , blank=True, null=True)
+   
+
+    def __str__(self):
+        return str(self.user)
+
+
+
+
+@receiver(post_save , sender=User)
+def create_user_profile(sender,instance,created , **kwargs):
+    if created:
+        Profile.objects.create(
+            user = instance
+        )
