1058aeb9983546675387d80c87289e3a70024554
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-08-24 11:46:48 +0100

    Login with Email in Django

--- project/settings.py
+++ project/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+LOGIN_REDIRECT_URL = '/accounts/profile'
+# AUTH_USER_MODEL = 'accounts.Profile'
+AUTHENTICATION_BACKENDS = ['accounts.backends.EmailBackend'] # new
