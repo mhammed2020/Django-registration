e02eeb6768e0bc57bb0537202e65960c7e004c7f
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-08-24 10:45:24 +0100

    create profile edit

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,14 +9,14 @@ class SignUpForm(UserCreationForm):
         model = User
         fields = ['username','email','password1','password2']
 
-
+#####################################
 class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
         fields = ['username','email','first_name','last_name']
 
-
+####################################
 class ProfileForm(forms.ModelForm):
     
     class Meta:
--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import authenticate, login
 # Create your views here.
 
 def sign_up(request):
-    if request.method == 'POST' : #save form
+    if request.method == 'POST' : #save SignUpForm
         form = SignUpForm(request.POST)
         if  form.is_valid():
             form.save()
@@ -13,13 +13,9 @@ def sign_up(request):
             password = form.cleaned_data['password1']
             user = authenticate(username = username, password = password)
             login(request,user)
-
             return redirect('/accounts/profile')
 
-
-
-
-    else : #show form SignUpForm
+    else : #show  SignUpForm
         form = SignUpForm()
     context = {'form':form}
     return render(request,'registration/signup.html',context)
@@ -33,18 +29,26 @@ def profile(request):
     return render(request,'profile/profile.html',{'profile':profile})
 
 
-
-
 def profile_edit(request):
+
+    profile = Profile.objects.get(user = request.user)
 
 
     if request.method == 'POST':
-        userform = UserForm(request.POST)
-        profileform = ProfileForm(request.POST)
+        userform = UserForm(request.POST,instance =request.user)
+        profileform = ProfileForm(request.POST,instance =profile)
+
+        if userform.is_valid() and profileform.is_valid():
+            userform.save()
+
+            myform = profileform.save(commit = False)
+            myform.user = request.user
+            myform.save()
+            return redirect('/accounts/profile')
 
     else :
-        userform = UserForm()
-        profileform = ProfileForm()
+        userform = UserForm(instance = request.user)
+        profileform = ProfileForm(instance = profile)
 
     return render(request,'profile/profile_edit.html',
     {'userform':userform,
